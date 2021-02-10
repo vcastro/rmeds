b32844cc8db6d9e6e3b672c6cecf4e73847d6a59
Victor Castro <victormcastrojr@gmail.com>
2021-02-10 09:24:54 -0500

    update hierarchy

--- README.md
+++ README.md
@@ -5,48 +5,30 @@
 
 <!-- badges: start -->
 
+[![R build
+status](https://github.com/vcastro/rmeds/workflows/R-CMD-check/badge.svg)](https://github.com/vcastro/rmeds/actions)
 <!-- badges: end -->
 
-The goal of rmeds is to …
+The goal of rmeds is to map medications from electronic health record
+(EHR) and claims datasets to RXNORM ingredients and also to various drug
+classes
 
 ## Installation
 
-You can install the released version of rmeds from
-[CRAN](https://CRAN.R-project.org) with:
+You can install the development version of rmeds from GitHub with:
 
 ``` r
-install.packages("rmeds")
+# install.packages("devtools")
+devtools::install_github("vcastro/rmeds")
 ```
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+This is a basic example to load the full list of RXNORM ingredients:
 
 ``` r
 library(rmeds)
-## basic example code
+
+## load list of ingredients
+data("rxnorm_in")
 ```
-
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
-```
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date.
-
-You can also embed plots, for example:
-
-<img src="man/figures/README-pressure-1.png" width="100%" />
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub\!
--- data-raw/classes.R
+++ data-raw/classes.R
@@ -1,6 +1,8 @@
+library(tidyverse)
+
 # classes
 
-class_graph <- read_delim("data-raw/19c09ef9cecef6e384b1d762c941f416.text", delim="|",
+class_graph <- read_delim("data-raw/rxmix/19c09ef9cecef6e384b1d762c941f416.text", delim="|",
                          col_types = cols(.default = col_character()),
                          col_names = FALSE)
 
@@ -15,40 +17,72 @@ class_names <- class_graph %>%
 
 class_hierachy <- class_edges %>%
   inner_join(class_names, by = c("classId1" = "classId")) %>%
-  inner_join(class_names, by = c("classId2" = "classId")) %>%
+  left_join(class_names, by = c("classId2" = "classId")) %>%
   select(classId = classId1,
          parent_classId = classId2,
          className = name.x,
          parent_className = name.y,
          classType = classType.x) %>%
+  filter(className != parent_className) %>%
   unique()
 
 
 ### save batch config files: EPC, VA, ATC, graphs
 
+classtree <- class_hierachy %>%
+  filter(classType == "VA") %>%
+  select(parent = parent_className,
+         child = className,
+         classId) %>%
+  #data.frame() %>%
+  data.tree::FromDataFrameNetwork() %>%
+  #data.tree::ToDataFrameTypeCol('classId') %>%
+  #mutate(c_fullname = paste(level_1, level_2, level_3, level_4, sep="/"))
 
+  data.tree::ToListExplicit(unname = TRUE) %>%
+networkD3::radialNetwork(fontSize = 7)
+
+
+#INTRODUCTION
+#INVESTIGATIONAL AGENTS
+#REAGENTS
+
+
+?networkD3::radialNetwork
+data.tree::as.Node.data.frame(class_hierachy[,c("className", "parent_className")])
 
 ##EPC
-epc_classMembers <- read_delim("data-raw/41cdc77be8391ab91175c31dafbd0193.text",
+epc_class_in <- read_delim("data-raw/rxmix/41cdc77be8391ab91175c31dafbd0193.text",
                                delim="|",
                                col_types = cols(.default = col_character()),
                                col_names = paste0("X", 1:24)) %>%
-  select(RXCUI = X15, STR = X14, classId = X20, className = X22, classType = X24) %>%
+  select(RXCUI = X16, STR = X14, classId = X20, className = X22, classType = X24) %>%
   filter(!is.na(RXCUI)) %>%
   unique()
 
+
 ##VA
-va_classMembers <- read_delim("data-raw/4f3b501e3935992139dd09943827a427.text", delim="|",
+va_classMembers <- read_delim("data-raw/rxmix/4f3b501e3935992139dd09943827a427.text", delim="|",
                                col_types = cols(.default = col_character()),
                                col_names = paste0("X", 1:24)) %>%
   filter(X14 == "has_VAClass") %>%
   select(RXCUI = X12, STR = X10, TTY = X8, classId = X16, className = X18, classType = X6) %>%
   unique()  #MAP TO IN
 
+
+va_classMembers %>%
+  group_by(RXCUI) %>%
+  count(sort =TRUE) %>% filter(n > 1)
+
 ##ATC
-atc_classMembers <- read_delim("data-raw/64022166ca33a965e91bbe0d2e710fec.text", delim="|",
+
+atc_class_in <- read_delim("data-raw/rxmix/64022166ca33a965e91bbe0d2e710fec.text", delim="|",
                               col_types = cols(.default = col_character()),
                               col_names = paste0("X", 1:24)) %>%
   filter(X6 == "ATC") %>%
   select(RXCUI = X12, STR = X10, TTY = X8, classId = X16, className = X18, classType = X6) %>%
   unique()
+
+epc_classMembers %>%
+group_by(RXCUI) %>%
+  count(sort = TRUE) %>% filter(n > 1)
